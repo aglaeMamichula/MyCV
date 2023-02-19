187b1c140826ca991e79dfc8d807c4c4a65bb4ee
aglae mamichula <aglae12345@yahoo.com>
2023-02-19 11:22:50 +0000

    custom site

--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-  
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("projects/", views.projects, name="projects"),
-    path("contact/", views.contact, name="contact"),
-]
